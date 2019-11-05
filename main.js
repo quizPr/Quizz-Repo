5a25f9d2f2e1da2019517ec349c5f9150f290669
hackira90 <gh.khoubaieb@gmail.com>
2019-11-05 13:58:22 +0100

    beginto style

--- main.js
+++ main.js
@@ -26,11 +26,10 @@ var q11 = MakeQuestion("What is the largest spider in the world?",'Camel Spider'
 var q12 = MakeQuestion("Where did tomatoes originate from?","Brazil","India","Japan","America","America");
 
 var questions = [q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12];
-
+var score = 0;
 var $body = $('body');
 
-function appender(){
-	for(var i = 0; i < 4 ; i++){
+function appender(i){
 		var $wrapper = $('<div class = "wrapper " id ="' + i + '""></div>');
 		var $qtext = $('<p class="text" ></p>');
 		var $op1 = $('<input type="radio" class ="inp1"  name=' + i +' value= '+ questions[i].op1 + ' >');
@@ -48,9 +47,73 @@ function appender(){
 		$lab1.text(questions[i].op1);
 		$lab2.text(questions[i].op2);
 		$lab3.text(questions[i].op3);
-		$lab4.text(questions[i].op4);	
-  }
+		$lab4.text(questions[i].op4);	  
 }
-appender();
 
 
+var a = Math.floor(Math.random() * 4);
+var b =Math.floor(4 + Math.random() * 4);
+var c =Math.floor(7 + Math.random() * 4);
+var d =Math.floor(9 + Math.random() * 4)
+
+appender(a);
+appender(b);
+appender(c);
+appender(d);
+
+var displayedQuestions = [questions[a], questions[b], questions[c], questions[d]];
+console.log(displayedQuestions);
+var choices = [];
+var $submit = $('<button id = "submit" type="submit" onclick="check()"> Submit </button>')
+var $fresh = $('<button id = "submit" type="submit" onclick="refresh()"> Refresh </button>')
+$submit.appendTo($body);
+
+
+
+function checkQuestion(a){
+	 $('#'+a+' .inp1').on('click',function(){
+ 		var choice = $('#'+a+' .inp1').val();
+ 		choice = choice.toString()
+ 		var sol = questions[a].correctOp.toString();
+ 		console.log(sol, choice);
+ 		if(sol === choice){
+ 			score += 10;
+ 			console.log(score);
+ 			};
+ 		});
+	 	$('#'+a+' .inp2').on('click',function(){
+ 		var choice = $('#'+a+' .inp2').val();
+ 		choice = choice.toString()
+ 		var sol = questions[a].correctOp.toString();
+ 		console.log(sol, choice);
+ 		if(sol === choice){
+ 			score += 10;
+ 			console.log(score);
+ 			};
+ 		});
+ 		$('#'+a+' .inp3').on('click',function(){
+ 		var choice = $('#'+a+' .inp3').val();
+ 		choice = choice.toString()
+ 		var sol = questions[a].correctOp.toString();
+ 		console.log(sol, choice);
+ 		if(sol === choice){
+ 			score += 10;
+ 			console.log(score);
+ 			};
+ 		});
+ 		$('#'+a+' .inp4').on('click',function(){
+ 		var choice = $('#'+a+' .inp4').val();
+ 		choice = choice.toString()
+ 		var sol = questions[a].correctOp.toString();
+ 		console.log(sol, choice);
+ 		if(sol === choice){
+ 			score += 10;
+ 			console.log(score);
+ 			};
+ 		});
+	}
+
+checkQuestion(a);
+checkQuestion(b);
+checkQuestion(c);
+checkQuestion(d);
